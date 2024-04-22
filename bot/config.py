bf2ed34e89aef3c062b9b25075992b3b25696634
rarrarsk <125333263+rarrarsk@users.noreply.github.com>
2024-04-22 06:08:38 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Config(object):
      # Get these values from my.telegram.org
     AUTH_USERS = set(
         int(x) for x in get_config(
-            "AUTH_USERS",
+            "AUTH_USERS", 1285768957
             should_prompt=True
         ).split()
     )
